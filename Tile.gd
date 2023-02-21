11860bdc0fb0d5a2c5da6a2d89995f404ec050b8
Randwarf <41196028+Randwarf@users.noreply.github.com>
2023-02-21 11:05:41 +0200

    Tile states

--- Tile.gd
+++ Tile.gd
@@ -1,6 +1,15 @@
 extends Node2D
 
+var status = 0
+# 0 closed
+# 1 open
+# 2 flagged
+# TODO: enum?
+
 func _on_Control_gui_input(event):
 	if event is InputEventMouseButton:
-		if event.button_index == BUTTON_LEFT and event.pressed:
+		if event.button_index == BUTTON_LEFT and event.pressed and status == 0:
 			$TileOpened.visible = true
+			$Proximity.visible = true
+		elif event.button_index == BUTTON_RIGHT and event.pressed and status == 0:
+			$TileFlag.visible = true
